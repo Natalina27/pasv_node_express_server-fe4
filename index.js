ff36742bfb0adb67e44d2864ddbb629b6ddb8652
Natalya Myunster <natalyamyunster@yahoo.com>
2020-09-07 10:31:46 +0200

    configurations ws

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 
 const PORT = 5000;
 
 app.get('/', home);
-app.get('/info',info);
+app.get('/info', info);
 
-function home(req, res){
-    res.send('PASV.')
+function home(req, res) {
+    res.send('PASV.');
 }
-function info (req, res){
+
+function info(req, res) {
     const a = 123;
     const b = Math.random();
     const c = a + b;
     console.log(c);
-    res.send('INFO here!')
+    res.send('INFO here!');
 }
-
 
 
 app.listen(PORT, () => {
