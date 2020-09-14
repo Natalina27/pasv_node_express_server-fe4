c714c7bceea303d192fda62a037c24da9169d42d
Natalya Myunster <natalyamyunster@yahoo.com>
2020-09-14 20:51:11 +0200

    some changes eslint , babel etc

--- src/modules/core/cors.js
+++ src/modules/core/cors.js
@@ -3,9 +3,9 @@ export default function cors(app) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
       'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorisation',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
     );
-    if (req.method === 'OPTION') {
+    if (req.method === 'OPTIONS') {
       res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
       return res.status(200).json({});
     }
