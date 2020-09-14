2939537ce47b8cc1c9515e6945285f28ea603ddd
Natalya Myunster <natalyamyunster@yahoo.com>
2020-09-14 23:55:38 +0200

    Mongoose connection

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ https://docs.mongodb.com/manual/tutorial/install-mongodb-on-os-x/
 https://github.com/pasv-crm/crmServer/blob/master/README.md
 
 to stop mongo : ctrl+C
+
+###Mongoose
+https://mongoosejs.com/docs/index.html
---
+++ src/modules/core/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+function dbConnect() {
+  mongoose.connect('mongodb://localhost/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function () {
+    console.log('CONNECT');
+  });
+}
+
+export default dbConnect;
